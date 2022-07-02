591b2079f845f0442519679ca3a9300c2ae14cc5
jbettmann <jordan.bettmann@gmail.com>
2022-07-01 21:23:54 -0600

    implemented timer and logic

--- src/App.jsx
+++ src/App.jsx
@@ -4,52 +4,134 @@ import useCountDown from "react-countdown-hook";
 import "./App.css";
 
 function App() {
-  const [initialTime, setInitialTime] = useState(0);
+  let [initialTime, setInitialTime] = useState();
+  const [running, setRunning] = useState(false);
+  let [displayHours, setDisplayHours] = useState();
+  let [displayMins, setDisplayMins] = useState();
+  let [displaySeconds, setDisplaySeconds] = useState();
+
+  let [hour, setHour] = useState();
+  let [minute, setMinute] = useState();
+  let [second, setSecond] = useState();
+
   console.log("on first mount", initialTime);
   let interval;
-  let [time, setTime] = useState();
   const [timeLeft, { start, pause, resume, reset }] = useCountDown(
-    time,
+    initialTime,
     interval
   );
-  console.log("on mounts", time);
+  console.log("on mounts", initialTime);
 
   let hours = Math.floor((timeLeft % (24 * 60 * 60 * 1000)) / (1000 * 60 * 60));
   let minutes = Math.floor((timeLeft % (60 * 60 * 1000)) / (1000 * 60));
   let seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
 
-  let displayHours = hours < 10 ? `0${hours}` : hours;
-  let displayMins = minutes < 10 ? `0${minutes}` : minutes;
-  let displaySeconds = seconds < 10 ? `0${seconds}` : seconds;
+  let showHours = hours < 24 ? `0${hours}` : hours;
+  let showMins = minutes < 10 ? `0${minutes}` : minutes;
+  let showSeconds = seconds < 10 ? `0${seconds}` : seconds;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let total = hour + minute + second;
+    setInitialTime(total);
+    setRunning(true);
+    start(total);
+  };
 
   return (
     <div className="App">
-      <p>
-        {displayHours} Hours : {displayMins}
-        Mins : {displaySeconds} Secs
-      </p>
-      <button title="Start" onClick={() => start()}>
-        Start
-      </button>
-      <button title="Stop" onClick={() => pause()}>
-        Stop
-      </button>
-      <button title="resume" onClick={() => resume()}>
+      {running ? (
+        <p>
+          {showHours} Hours : {showMins}
+          Mins : {showSeconds} Secs
+        </p>
+      ) : (
+        <p>
+          {displayHours < 24 ? `0${displayHours}` : displayHours} Hours :
+          {displayMins < 10 ? `0${displayMins}` : displayMins}
+          Mins : {displaySeconds < 10 ? `0${displaySeconds}` : displaySeconds}
+          Secs
+        </p>
+      )}
+      {!running ? (
+        <button
+          title="Start"
+          onClick={() => {
+            start(initialTime);
+            setRunning((e) => !e);
+          }}
+        >
+          Start
+        </button>
+      ) : (
+        <button
+          title="Stop"
+          onClick={() => {
+            pause();
+            setRunning((e) => !e);
+          }}
+        >
+          Stop
+        </button>
+      )}
+      <button
+        title="resume"
+        onClick={() => {
+          resume();
+          setRunning(true);
+        }}
+      >
         Resume
       </button>
-      <button title="Reset" onClick={() => reset()}>
+      <button
+        title="Reset"
+        onClick={() => {
+          reset();
+          setRunning(false);
+        }}
+      >
         Reset
       </button>
-
-      <form>
-        <input
-          type="text"
-          value={time}
-          onChange={(e) => setTime(e.target.value)}
-          placeholder="hours"
-        />
-        <input type="submit" value="Submit"></input>
-      </form>
+      {!initialTime && !running ? (
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="hours"
+            onChange={(e) => {
+              setDisplayHours(
+                e.target.value %
+                  Math.floor((24 * 60 * 60 * 1000) / (1000 * 60 * 60))
+              );
+              setHour(e.target.value * 60 * 60 * 1000);
+              console.log(hour);
+            }}
+          />
+          <input
+            type="text"
+            name="mins"
+            onChange={(e) => {
+              setDisplayMins(e.target.value % Math.floor(60 * 60 * 1000)) /
+                (1000 * 60);
+              setMinute(e.target.value * 60 * 1000);
+              console.log(minute);
+            }}
+          />
+          <input
+            type="text"
+            name="seconds"
+            onChange={(e) => {
+              setDisplaySeconds(
+                e.target.value % Math.floor((1000 * 60) / 1000)
+              );
+              setSecond(e.target.value * 1000);
+              console.log(second);
+            }}
+          />
+          <button type="submit">Start</button>
+        </form>
+      ) : (
+        <div></div>
+      )}
     </div>
   );
 }
