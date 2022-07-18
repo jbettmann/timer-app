073c2cc1f9bfd41712f114c0f24b90f780a270f2
jbettmann <jordan.bettmann@gmail.com>
2022-07-18 16:26:47 -0600

    created new timer modal with name state

---
+++ src/components/new-timer-modal/new-timer-modal.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react";
+import {
+  Button,
+  TextField,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from "@mui/material";
+import { Timer } from "../timer/timer";
+
+export const NewTimerModal = ({ open, handleClose, addNewTimer }) => {
+  const [name, setName] = useState("");
+
+  const updateTimerName = (e) => {
+    let value = e.target.value;
+    setName(value);
+  };
+
+  const submit = () => {
+    addNewTimer(name);
+    handleClose();
+    setName("");
+  };
+
+  return (
+    <Dialog open={open} onClose={handleClose}>
+      <DialogContent>
+        <DialogContentText>{name}</DialogContentText>
+        <TextField
+          autoFocus
+          margin="dense"
+          id="name"
+          label="Name Timer"
+          type="text"
+          onChange={(event) => updateTimerName(event)}
+          fullWidth
+        />
+      </DialogContent>
+      <Timer />
+      <DialogActions>
+        <Button onClick={handleClose}>Close</Button>
+        <Button onClick={submit}>Start</Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
