0af43dd41b10113d6170a65b4325d9bff32ad7aa
jbettmann <jordan.bettmann@gmail.com>
2022-08-25 15:15:15 -0600

    updated styling on times-up modal

--- src/components/times-up/times-up.jsx
+++ src/components/times-up/times-up.jsx
@@ -6,13 +6,15 @@ import {
   DialogContentText,
 } from "@mui/material";
 
+import "./timers-up.css";
+
 export const TimesUp = ({ open, name, handleTimesUpClose, handleRepeat }) => {
   return (
-    <Dialog open={open} onClose={handleTimesUpClose}>
+    <Dialog id="times-up__main" open={open} onClose={handleTimesUpClose}>
       <DialogContent>
         <DialogContentText>{name} Finished!</DialogContentText>
       </DialogContent>
-      <DialogActions>
+      <DialogActions id="times-up__buttons">
         <Button onClick={handleRepeat}>Repeat</Button>
         <Button onClick={handleTimesUpClose}>Close</Button>
       </DialogActions>
